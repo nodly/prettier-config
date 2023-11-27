8d332d7edbb95032b84c2ec29e6dfecdf2cc69c3
Marcus Lilja <marcus.lilja@gmail.com>
2023-11-27 23:10:35 +0100

    feat: add shared plugins

--- src/prettier.config.ts
+++ src/prettier.config.ts
@@ -1,7 +1,31 @@
-import { Config } from 'prettier';
+import type { PluginConfig as SortImportsConfig } from '@ianvs/prettier-plugin-sort-imports';
+import type { Config as PrettierConfig } from 'prettier';
+import type { PluginOptions as TailwindConfig } from 'prettier-plugin-tailwindcss';
+import type { ReadonlyDeep } from 'type-fest';
+
+export type Config = PrettierConfig & TailwindConfig & SortImportsConfig;
 
 const config = {
   singleQuote: true,
-} as const satisfies Config;
+
+  plugins: [
+    '@ianvs/prettier-plugin-sort-imports',
+    'prettier-plugin-tailwindcss',
+  ],
+
+  importOrder: [
+    '<TYPES>',
+    '<TYPES>^[.]',
+    '',
+    '<BUILT_IN_MODULES>',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '^@/(.*)$',
+    '',
+    '^[.]',
+  ],
+
+  tailwindFunctions: ['clsx'],
+} as const satisfies ReadonlyDeep<Config>;
 
 export default config;
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import dts from 'vite-plugin-dts';
 import { resolve } from 'path';
+
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   build: {
